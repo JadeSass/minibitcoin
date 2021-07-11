bc05c4cbede634c1c7e40b5da44b2ed2e9ccef16
Jade Sass <48350133+JadeSass@users.noreply.github.com>
2021-07-11 13:42:54 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -148,6 +148,7 @@
             </div>
         </div>
         <!-- Compiled and minified JavaScript -->
+        <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>          
         <script src="{{asset('js/particles.min.js')}}"></script>
         <script src="{{asset('js/app.js')}}"></script>
