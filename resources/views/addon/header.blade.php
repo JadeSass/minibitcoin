d00e6bdcb09571f0c79e7e1bb6d1a21637a8b0b1
Jade Sass <48350133+JadeSass@users.noreply.github.com>
2021-07-11 21:57:23 +0100

    Update header.blade.php

--- resources/views/addon/header.blade.php
+++ resources/views/addon/header.blade.php
@@ -1,4 +1,4 @@
-<header class="navbar-fixed">
+4r class="navbar-fixed">
 <nav class="white">
     <div class="nav-wrapper container">
       <a href="#!" class="brand-logo orange-text text-darken-3"><img src="{{asset('img/logo.png')}}" alt="" width="65px" height="65px"></a>
@@ -23,5 +23,5 @@
     <h6 class="center-align black-text"><b>Join our community</b></h6>
     <li><a href="https://t.me/mini_btc"><i class="fa fa-telegram fa-2x blue-text text-lighten-1"></i> Telegram Group</a></li>
     <li><a href="#"><i class="fa fa-facebook fa-2x blue-text text-darken-3"></i> Facebook Page</a></li>
-    <li><a href="#"><i class="fa fa-twitter fa-2x blue-text text-lighten-3"></i> Twitter Profile</a></li>
+    <li><a href="https://twitter.com/btc_mini?s=21"><i class="fa fa-twitter fa-2x blue-text text-lighten-3"></i> Twitter Profile</a></li>
   </ul>
