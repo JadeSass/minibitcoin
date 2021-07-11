0f64bc6893e90bd0fa342445394fe5de690946dd
Jade Sass <48350133+JadeSass@users.noreply.github.com>
2021-07-11 14:57:12 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -52,9 +52,9 @@
                     <h4 class="orange-text text-darken-3 center-align"><b>THE TIMELINE</b></h4>
                     <h6 class="orange-text text-darken-3 center-align" style="line-height:1.9rem;"><b>Here's our timeline, some of these dates may change however due to market conditions..</b><br><br></h6>
                     <div class="carousel">
-                        <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>
-                        <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"></a>
-                        <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/250/250/nature/3"></a>
+                        <a class="carousel-item" href="#one!"><img src="{{asset('img/')}}"></a>
+                        <a class="carousel-item" href="#two!"><img src="{{asset('img/')}}"></a>
+                        <a class="carousel-item" href="#three!"><img src="{{asset('img/')}}"></a>
                     </div>
                 </div>
             </div>
